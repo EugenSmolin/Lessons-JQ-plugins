9a18f743a5b3c0ca8634075e3756af7af76810ec
Евгений Смолин <Tonylaureano665@gmail.com>
2018-05-23 09:52:52 +0300

    for

--- JavaScript - решает(практика)/10-Циклы/script.js
+++ JavaScript - решает(практика)/10-Циклы/script.js
@@ -1,10 +1,13 @@
 // циклы (for)
-/*var result = document.getElementById('result');
+/*
+var result = document.getElementById('result');
 for (var i = 1; i <= 9; i++) {
 	result.innerHTML += '7 * ' + i + ' = ' + 7*i + '<br>';
-}*/
-
-var result = document.getElementById('result');
-for (var i = 1000; i <= 2000; i++) {
-	result.innerHTML += '&#' + i + '<br>';
 }
+*/
+/*
+var result = document.getElementById('result');
+for (var i = 7000; i <= 10000; i++) {
+	result.innerHTML += '&#' + i + ' ';
+}
+*/
